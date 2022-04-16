6d1a3e00d6756467320310084b8ce301b6982ec2
leonardo-schneider2 <leocoelho256@gmail.com>
2022-04-16 15:47:18 -0300

    finalizando, falta completar o ESLINT - 12 problems (12 errors, 0 warnings)

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,83 +1,51 @@
-//Desafio 11
+// Desafio 11
 function generatePhoneNumber(numbers) {
   if (numbers.length !== 11) {
     // teste 11 numeros
-    return "Array com tamanho incorreto.";
-  } 
-  
-  
-  else {
-    for (let i = 0; i < numbers.length; i++) {
-      // teste repeticao mais que 3 vezes, ou maior que 9 ou negativo
-      let repete = 0 // zera repeticao para comparar um por um 
-        for (let j = 0; j < numbers.length; j++) {
-          if (numbers[i] == numbers[j]) {
-            repete ++;
-          }
-          if (repete >= 3 || numbers[j] > 9 || numbers[j] < 0) {
-            return 'não é possível gerar um número de telefone com esses valores';
-          }
-        }
-    }
-    // Já comparamos, agora continuamos a montar o número
-
-    let resultado = [];
-    /*
-    resultado.push('(');
-    resultado.push(numbers[0]);
-    resultado.push(numbers[1]);
-    resultado.push(') ');
-    resultado.push(numbers[2]);
-    resultado.push(numbers[3]);
-    resultado.push(numbers[4]);
-    resultado.push(numbers[5]);
-    resultado.push(numbers[6]);
-    resultado.push('-');
-    resultado.push(numbers[7]);
-    resultado.push(numbers[8]);
-    resultado.push(numbers[9]);
-    resultado.push(numbers[10]);
-    */
-    for (let i = 0; i < numbers.length; i++){
-      switch (i){
-        case 0:
-          resultado.push('(');
-        break;
-        case 2:
-          resultado.push(') ');
-        break;
-        case 7:
-          resultado.push('-');
-        break;
-        default:
-        break;
-      }
-      resultado.push(numbers[i]);
-
-    }
-
-    return resultado.join("");
+    return 'Array com tamanho incorreto.';
   }
-
+  for (let i = 0; i < numbers.length; i += 1) {
+    // teste repeticao mais que 3 vezes, ou maior que 9 ou negativo
+    let repete = 0;
+    for (let j = 0; j < numbers.length; j += 1) {
+      if (numbers[i] === numbers[j]) {
+        repete += 1;
+      }
+      if (repete >= 3 || numbers[j] > 9 || numbers[j] < 0) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+  }
+  let resultado = [];
+  for (let i = 0; i < numbers.length; i += 1) {
+    switch (i) {
+    case 0:
+      resultado.push('(');
+      break;
+    case 2:
+      resultado.push(') ');
+      break;
+    case 7:
+      resultado.push('-');
+      break;
+    default:
+      break;
+    }
+    resultado.push(numbers[i]);
+  }
+  return resultado.join('');
 }
-
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
-  if ((lineA > lineB + lineC) || (lineB > lineA + lineC) || (lineC > lineB + lineA)){
-    
+  let A = lineA;
+  let B = lineB;
+  let C = lineC;
+  if ((A > B + C) || (B > A + C) || (C > B + A)) {
     return false;
-
-  } else if ((lineA < Math.abs(lineC - lineB)) || (lineB < Math.abs(lineA - lineC)) || (lineC < Math.abs(lineB - lineA))){
-    
-    return false;
-
-  } else {
-
-    return true;
-
   }
 
+  return true;
 }
 
 // Desafio 13
@@ -86,15 +54,15 @@ function hydrate(bebidas) {
   let reg = /\d+/g;
   let copos = bebidas.match(reg);
   let somaCopos = 0;
-  for (let i = 0; i < copos.length; i++){
-    somaCopos += parseInt(copos[i]);
+  for (let i = 0; i < copos.length; i += 1) {
+    somaCopos += parseInt(copos[i], 10);
   }
   if (copos.length === 1) {
-    return (somaCopos +' copo de água');
-  } else {
-    return (somaCopos + ' copos de água');
+    let resposta = `${somaCopos} copo de água`;
+    return resposta;
   }
-  
+  let resposta = `${somaCopos} copos de água`;
+  return resposta;
 }
 
 module.exports = {
